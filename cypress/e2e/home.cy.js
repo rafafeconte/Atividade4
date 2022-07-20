8c0c642c68c9a6c51ce98e1741cb974d9a841ba2
rafael <rafael.conte@webjump.com.br>
2022-07-20 15:06:43 -0300

    pasta factories

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,6 +2,6 @@ describe('home page', ()=>{
     it('Visitar home', ()=>{
         cy.visit('https://magento.nublue.co.uk/')
         cy.get('div[class="panel header"] ul li a[href="https://magento.nublue.co.uk/customer/account/create/"]').should('have.text', 'Create an Account')
-fdfd
+
     }) 
 })
